8aab792986ba24aa5a8e18d0d4c66d569453006e
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-10-16 13:19:00 +0200

    finish set up

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDh5eUrTHh4gq8mlE2j0asq8vI2UWz3nS4",
+  authDomain: "cryptohub-6006f.firebaseapp.com",
+  projectId: "cryptohub-6006f",
+  storageBucket: "cryptohub-6006f.appspot.com",
+  messagingSenderId: "769102355080",
+  appId: "1:769102355080:web:83bbd6408deff042485d3d"
+};
+
+
+const app = initializeApp(firebaseConfig);
