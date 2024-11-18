34e7093bd38476c602af9accf024c618ac99f7cb
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-11-18 17:36:02 +0100

    fix footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="mt-16 px-8 pb-5 grid grid-cols-1 gap-y-3 md:grid md:grid-rows-2 md:grid-cols-2 md:justify-items-evenly	">
       <div
         id="links"
-        className="grid grid-cols-1 md:row-start-1  md:row-end-3  md:justify-items-center "
+        className="grid grid-cols-1 justify-items-center md:row-start-1  md:row-end-3  "
       >
         <Link
           to="/"
@@ -30,7 +30,7 @@ const Footer = () => {
           FAQs
         </Link>
       </div>
-      <div id="contacts" className="md:justify-self-center">
+      <div id="contacts" className="justify-self-center">
         <a className="flex" href="tel:+777777777">
           <PhoneIcon
             svgStyles="size-6 fill-none stroke-elements-main stroke-2"
@@ -51,7 +51,7 @@ const Footer = () => {
           </span>
         </a>
       </div>
-      <div id="features" className="md:justify-self-center">
+      <div id="features" className="justify-self-center">
         <ThemeSwitcher stylesButton="hover:fill-accent-color" />
       </div>
     </footer>
