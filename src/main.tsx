dd40f75105a57b4821b925e150bfbbf6c8f72a2f
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-11-22 14:58:30 +0100

    add redux, persist setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { store, persistor } from "./redux/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="/">
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
